26d6c184d554e00b75a4d428ad745204a63204be
w4kened <w4kened@mailfence.com>
2025-01-30 20:24:38 +0100

    Plant test application created

---
+++ Plant/src/main/java/com/example/Plant/PlantRepository.java
@@ -0,0 +1,13 @@
+package com.example.Plant;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface PlantRepository extends CrudRepository<Plant, Integer> {
+}
