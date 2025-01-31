6cd68f8a13ddf0eb08d0b6c68c4613f08a5c2399
w4kened <w4kened@mailfence.com>
2025-01-31 16:33:56 +0100

    flexible custom finding queries added

--- Plant/src/main/java/com/example/Plant/PlantController.java
+++ Plant/src/main/java/com/example/Plant/PlantController.java
@@ -24,28 +24,22 @@ public class PlantController {
     public PlantController(final PlantRepository plantRepository) {
         this.plantRepository = plantRepository;
     }
-
     @GetMapping("/plants")
     public Iterable<Plant> getAllPlants() {
         return this.plantRepository.findAll();
     }
-
     @GetMapping("/plants/{id}")
     public Optional<Plant> getPlantById(@PathVariable("id") Integer id) {
         return this.plantRepository.findById(id);
     }
-
     @PostMapping("/plants")
     public Plant createNewPlant(@RequestBody Plant plant) {
         Plant newPlant = this.plantRepository.save(plant);
         return newPlant;
     }
-
     @PutMapping("/plants/{id}")
-    public Plant updatePlant(
-            @PathVariable("id") Integer id,
-            @RequestBody Plant p
-    ) {
+    public Plant updatePlant(@PathVariable("id") Integer id,
+                             @RequestBody Plant p) {
         Optional<Plant> plantToUpdateOptional = this.plantRepository.findById(id);
         if (!plantToUpdateOptional.isPresent()) {
             return null;
@@ -66,7 +60,6 @@ public class PlantController {
         Plant updatedPlant = this.plantRepository.save(plantToUpdate);
         return updatedPlant;
     }
-
     @DeleteMapping("/plants/{id}")
     public Plant deletePlant(@PathVariable("id") Integer id) {
         Optional<Plant> plantToDeleteOptional = this.plantRepository.findById(id);
@@ -76,5 +69,29 @@ public class PlantController {
         Plant plantToDelete = plantToDeleteOptional.get();
         this.plantRepository.delete(plantToDelete);
         return plantToDelete;
+    }
+    @GetMapping("/plants/search")
+    public List<Plant> searchPlants(
+            @RequestParam(name="hasFruit", required = false) Boolean hasFruit,
+            @RequestParam(name="maxQuantity", required=false) Integer quantity
+    ) {
+        if (hasFruit != null && quantity != null &&
+                hasFruit.equals(true)) {
+            return this.plantRepository.findByHasFruitTrueAndQuantityLessThan(quantity);
+        }
+        else if (hasFruit != null && quantity != null &&
+                hasFruit.equals(false)) {
+            return this.plantRepository.findByHasFruitFalseAndQuantityLessThan(quantity);
+        }
+        else if (hasFruit != null && hasFruit.equals(true)) {
+            return this.plantRepository.findByHasFruitTrue();
+        }
+        else if (hasFruit != null && hasFruit.equals(false)) {
+            return this.plantRepository.findByHasFruitFalse();
+        }
+        else if (quantity != null) {
+            return this.plantRepository.findByQuantityLessThan(quantity);
+        }
+        return new ArrayList<>();
     }
 }
--- Plant/src/main/java/com/example/Plant/PlantRepository.java
+++ Plant/src/main/java/com/example/Plant/PlantRepository.java
@@ -10,4 +10,9 @@ import org.springframework.data.repository.CrudRepository;
 
 
 public interface PlantRepository extends CrudRepository<Plant, Integer> {
+    List<Plant> findByHasFruitTrue();
+    List<Plant> findByHasFruitFalse();
+    List<Plant> findByQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitTrueAndQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitFalseAndQuantityLessThan(Integer quantity);
 }
